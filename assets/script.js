aa2362eb0c97cae38d4d19c024982dbc6c8ddca2
binhta <binhta102@gmail.com>
2022-04-02 19:44:23 -0400

    search city done

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,28 @@
 var searchBtn = document.querySelector("#searchButton")
 var searchCity = document.querySelector("#userSearch")
+var apiKey = "eb36efeec9e8db03dceda4aa497b1c1f"
+
+//console.log(searchCity)
+
+searchBtn.addEventListener("click", function(){
+    console.log(searchCity)
+    var requestUrl = 
+        "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity.value + "&appid=eb36efeec9e8db03dceda4aa497b1c1f";
+        
+        fetch(requestUrl)
+        .then(function(forecast){
+            return forecast.json();
+        })
+        .then(function(data) {
+          console.log(data) 
+          var city = data.city 
+        })
+        for( var i = 0; i < data.length; i++){
+            console.log(city)
+        }
+
+      console.log(searchBtn);  
+})
+
+
+
