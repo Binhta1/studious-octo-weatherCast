c11d7268baf86e24949c5795cd89aafddf50217e
binhta <binhta102@gmail.com>
2022-04-03 00:24:50 -0400

    display current weather

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,103 @@
 var searchBtn = document.querySelector("#searchButton")
 var searchCity = document.querySelector("#userSearch")
 var apiKey = "eb36efeec9e8db03dceda4aa497b1c1f"
-var cityInfo = document.querySelector("cityName")
-
-//console.log(searchCity)
-
-searchBtn.addEventListener("click", function(){
-    
-    //console.log(searchCity)
-
-    var requestUrl = 
-        "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity.value + "&appid=eb36efeec9e8db03dceda4aa497b1c1f";
-        
-        fetch(requestUrl)
-        .then(function(forecast){
-            return forecast.json();
-        })
-        .then(function(data) { 
-          console.log(data) 
-          //console.log(data.city)
-          console.log(data.city.name)
-          
-       
-        for( var i = 0; i < data.length; i++){
-          console.log(data)
-            var cityName = document.createElement("h2")
-            cityName.textContent = data[i].city.name;
-            cityName.appendChild(cityInfo)
-
-        }
-        });
-      console.log(searchBtn);  
-})
+var currentWeather = document.querySelector("#currentWeather")
+var forecast = document.querySelector("#forecast")
+var cityName =document.querySelector('#cityName')
+var forecastContainer = document.querySelector('#forecastContainer')
 
 
+var getCityWeather = function(currentCityInfo){
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCityInfo + "&appid="+ apiKey;
+        fetch(requestUrl).then(function(response) {
+            response.json().then(function(data) {   
+                console.log(data) 
+
+                var latitude = data.city.coord.lat;
+                var longitude = data.city.coord.lon;
+                var nameOfCity =data.city.name;
+                console.log(latitude)
+                console.log(longitude)
+                console.log(nameOfCity)
+
+                cityName.innerHTML= `<h3 class="current-city card-title">${nameOfCity}</h3>
+
+                `
+
+                var secondUrl ="https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon="+ longitude + "&exclude=minutely,hourly,alert&units=imperial&appid="+ apiKey;
+
+                fetch(secondUrl).then(function(response){
+                    response.json().then(function(data){
+                        console.log(data)
+
+                        displayCurrentWeather(data)
+                        //calling the data in current weather
+
+                    })
+                })
+           })   
+        })     
+    }
+
+    //pulling the data into the function
+function displayCurrentWeather(data) { 
+    const {icon} = data.current.weather[0];
+    const {temp, wind_speed, humidity, uvi} = data.current;
+    var colorBlock ="bg-danger";
+
+    currentWeather.innerHTML = `<div class="current-data card-body">
+        <img scr="https://openweathermap.org/img/wn/${icon}.png">
+        <div class = "card-text">Temp: ${temp}F</div>
+        <div class = "card-text">Wind: ${wind_speed}mph</div>
+        <div class = "card-text">Humidity: ${humidity}%</div>
+        <div class = "card-text">Wind: ${colorBlock}">UV Index:${uvi}</div>
+        </div>`;
+
+}
+
+
+
+function displayForecast(data) {
+    for (var i =1; i <= 5; i++) {
+        var weatherData = {
+            date: data.daily[i].dt,
+            Temp: data.daily[i].temp.day,
+            wind_speed: data.daily[i].wind_speed,
+            humidity: data.daily[i].humidity,
+            icon: data.current.weather[0].icon
+        };
+    var currentDate = moent.unix(weatherData.date).format("MM/DD/YYYY");
+
+    document.getElementById(
+        "forecast-" + i
+    ).innerHTML = `<div class="card" id="day-card:> 
+        <div><img src="https://openweathermap.org/img/wn/${weatherData.icon}.png"></div>
+        <div class = "card-text">Temp: ${weatherData.temp}°F</div>
+        <div class = "card-text">Wind: ${weatherData.wind_speed}mph</div>
+        <div class = "card-text">Humidity: ${weatherData.humidity}%</div>`
+    }
+}
+
+
+
+getCityWeather('tampa')
+
+
+
+
+
+
+
+
+
+
+// for( var i = 0; i < data.length; i++){
+//           console.log(data)
+//             var cityName = document.createElement('h2');
+//             cityName.textContent = data[i].city.name;
+//             cityInfo.append(cityName)
+
+//         }
+//         });
+      
 
